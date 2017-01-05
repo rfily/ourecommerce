af632fc031db4ea3be7f482dc2e7eb30d2b57c1e
rahrindra <rahrindra@gmail.com>
2017-01-05 13:58:48 +0300

    modification et suppression de categorie

--- src/Admin/AdminBundle/Controller/CategorieController.php
+++ src/Admin/AdminBundle/Controller/CategorieController.php
@@ -6,6 +6,8 @@ use Admin\AdminBundle\Entity\Categorie;
 use Admin\AdminBundle\Form\CategorieType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CategorieController extends Controller
 {
@@ -36,5 +38,46 @@ class CategorieController extends Controller
         return $this->render('@AdminAdmin/Categorie/ajouter.html.twig', array(
             'formajout' => $form->createView()
         ));
+    }
+
+    public function modifierAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $categorie = $em->getRepository('AdminAdminBundle:Categorie')->find($id);
+
+        if($categorie === null)
+        {
+            throw new NotFoundHttpException("La catégorie d'id ".$id." n'existe pas.");
+        }
+
+        $form = $this->createForm(new CategorieType(), $categorie);
+        $form->handleRequest($request);
+
+        if($form->isValid())
+        {
+            $em->flush();
+            $request->getSession()->getFlashBag()->add('notice', 'Catégorie bien modifiée.');
+            return $this->redirectToRoute('admin_admin_categorie');
+        }
+
+        return $this->render('AdminAdminBundle:Categorie:modifier.html.twig', array(
+            'form_modif' => $form->createView()
+        ));
+    }
+
+    public function supprimerAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $categorie = $em->getRepository('AdminAdminBundle:Categorie')->find($id);
+
+        if($categorie === null)
+        {
+            throw new NotFoundHttpException("La catégorie d'id ".$id." n'existe pas.");
+        }
+
+        $em->remove($categorie);
+        $em->flush();
+        $request->getSession()->getFlashBag()->add('notice', 'Catégorie bien supprimée');
+        return $this->redirectToRoute('admin_admin_categorie');
     }
 }
